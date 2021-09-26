507eb67ba1963994a593d3d039b422593ada964f
Zhanna <zhanna_sargsyan@edu.aua.am>
2021-09-26 17:53:34 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar/index";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Switch, Route } from "react-router-dom";
 import StickyFooter from "./containers/footer";
-<<<<<<< HEAD
-import TasksTable from "./components/TasksTable";
-
-function App() {
-  return (
-    <Switch>
-      <Route path="/">
-        {" "}
-        <Navbar />
-        <TasksTable />
-        <StickyFooter />
-      </Route>
-    </Switch>
-=======
 import ColumnSelectorGrid from "./components/TasksTable";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Switch>
         <Route path="/">
-          <Navbar />
-          <ColumnSelectorGrid />
-          <StickyFooter />
+          <div>
+            <Navbar />
+          </div>
+
+          <div>
+            <ColumnSelectorGrid />
+          </div>
+          <div>
+            <StickyFooter />
+          </div>
         </Route>
       </Switch>
-    </div>
->>>>>>> c8a2b11114f9cc5f4db300026d7829311f44411c
+    </>
   );
 }
 
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -8,36 +8,6 @@ import {
   NavBtn,
   NavBtnLink,
 } from "../navbarComponents";
-<<<<<<< HEAD
-import logo from "../pics/logo.svg";
-
-const Navbar = () => {
-  return (
-    <div>
-      <Nav>
-        <NavLink to="/">
-          <image src={logo} alt="" />
-        </NavLink>
-        <Bars />
-        <NavMenu>
-          <NavLink to="/Reported by me" activeStyle>
-            Reported by me
-          </NavLink>
-          <NavLink to="/ Assingned to me" activeStyle>
-            Assingned to me
-          </NavLink>
-          <NavLink to="/Create" activeStyle>
-            Create
-          </NavLink>
-        </NavMenu>
-        {
-          //   <NavBtn>
-          //     <NavBtnLink to="/create">Create</NavBtnLink>
-          //   </NavBtn>
-        }
-      </Nav>
-    </div>
-=======
 import Logo from "../pics/image2vector.svg";
 
 const Navbar = () => {
@@ -59,7 +29,6 @@ const Navbar = () => {
         </NavMenu>
       </Nav>
     </>
->>>>>>> c8a2b11114f9cc5f4db300026d7829311f44411c
   );
 };
 
